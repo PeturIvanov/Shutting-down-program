ee81e1b693c7c3749c104cfec7e0ad8631301ed6
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-05-03 21:30:19 +0300

    Update

--- code/timer_and_shutdown.py
+++ code/timer_and_shutdown.py
@@ -4,21 +4,30 @@ import time
 import datetime
 
 
-def shutdown_timer(mins):
-    seconds = mins * 60
-    jarvis.say(f"Shutdown after {mins} minutes!")
+
+def speak(hours_to_say, minutes_to_say):
+    if hours_to_say != 0:
+        if hours_to_say == 1:
+            jarvis.say(f"Shutdown after {hours_to_say} hour and {minutes_to_say} minutes")
+        else:
+            jarvis.say(f"Shutdown after {hours_to_say} hours and {minutes_to_say} minutes")
+    else:
+        jarvis.say(f"Shutdown after {minutes_to_say} minutes!")
     jarvis.runAndWait()
+
+
+def shutdown_timer(minutes, hours):
+    seconds = hours * 3600 + minutes * 60
+    speak(hours, minutes)
     while seconds > 0:
         timer = datetime.timedelta(seconds=seconds)
-        print(f"\r{timer}", end="")
+        print(f"\rTime remaining - {timer}", end="")
         time.sleep(1)
         seconds -= 1
     os.system("shutdown /s /t 1")
 
 
-# def stop_timer():
-
-
-minutes = int(input("Enter your timer:"))
-shutdown_timer(minutes)
+hours = int(input("Enter countdown hours:"))
+minutes = int(input("Enter countdown minutes:"))
+shutdown_timer(minutes, hours)
 
