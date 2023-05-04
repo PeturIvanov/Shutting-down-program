a5b697c2a0de08ee294b5e7155a947ee4a5f3187
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-05-04 13:57:13 +0300

    Update.

--- code/timer_and_shutdown.py
+++ code/timer_and_shutdown.py
@@ -5,6 +5,7 @@ import datetime
 import threading
 
 remaining_time = 0
+adding_time_seconds = 0
 
 
 def speak(hours_to_say, minutes_to_say):
@@ -35,9 +36,13 @@ def speak(hours_to_say, minutes_to_say):
 def shutdown_timer(hours, minutes):
     seconds = hours * 3600 + minutes * 60
     global remaining_time
+    global adding_time_seconds
     while seconds > 0:
         timer = datetime.timedelta(seconds=seconds)
         remaining_time = timer
+        if adding_time_seconds != 0:
+            seconds += adding_time_seconds
+            adding_time_seconds = 0
         time.sleep(1)
         seconds -= 1
     os.system("shutdown /s /t 1")
@@ -45,13 +50,17 @@ def shutdown_timer(hours, minutes):
 
 def menu():
     global remaining_time
+    global adding_time_seconds
     while True:
         print("For time remaining enter (t). For adding time enter(a)."
               " For stop timer enter(s). For pause the time enter(p)")
         user_input = input()
         if user_input == "t":
             print(f"Time remaining - {remaining_time}")
-
+        elif user_input == "a":
+            hours_to_add = int(input("Enter your hours to add:"))
+            minutes_to_add = int(input("Enter your minutes to add:"))
+            adding_time_seconds += (hours_to_add * 3600 + minutes_to_add * 60)
 
 
 remaining_hours = int(input("Enter countdown hours:"))
@@ -61,6 +70,7 @@ speak(remaining_hours, remaining_minutes)
 # tread for shutdown_timer.
 shutdown_timer_tread = threading.Thread(target=shutdown_timer, args=(remaining_hours, remaining_minutes))
 shutdown_timer_tread.start()
+
 
 # tread for menu.
 menu_thread = threading.Thread(target=menu)
